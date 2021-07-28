a2fdff2b2125514815a4497a190e437301f9d789
MarynaSavonenko <19901001@ukr.net>
2021-07-28 19:44:58 +0300

    d

--- js/tasck2.js
+++ js/tasck2.js
@@ -1,17 +1,15 @@
 const ingredients = [
-    'Картошка',
-    'Грибы',
-    'Чеснок',
-    'Помидоры',
-    'Зелень',
-    'Приправы',
-  ];
-//получаем доступ к списку 
-const ingredientsContainer = document.querySelector('#ingredients')
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+const ingredientsIdSearchEl = document.querySelector('#ingredients');
 
-ingredients.forEach(element => {
-   const listItem =  document.createElement('li')
-   listItem.textContent = element;
-   listItem.style.listStyle = 'none'
-   ingredientsContainer.appendChild(listItem)
+ingredients.forEach(ingredient =>{
+const ingredientsLiCreateEl = document.createElement('li');
+ingredientsLiCreateEl.textContent = ingredient;
+ingredientsIdSearchEl.append(ingredientsLiCreateEl)
 });
--- js/tasck4.js
+++ js/tasck4.js
@@ -2,12 +2,14 @@ const caunterContainer = document.querySelector('#counter');
 const btnDecrement = caunterContainer.querySelector('button[data-action="decrement"]')
 const btnIncrement = caunterContainer.querySelector('button[data-action="increment"]')
 const spanEl = caunterContainer.querySelector('#value');
-
+let value = 0
 
 btnDecrement.addEventListener('click', e => {
-    spanEl.textContent =  spanEl.textContent-1
+    value-=1
+    spanEl.textContent =  value
 })
 
 btnIncrement.addEventListener('click', e => {
-    spanEl.textContent =  +spanEl.textContent + 1
+    value+=1
+    spanEl.textContent =  value
 })
